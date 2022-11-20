5e1a768cf7dc056a13f77f9461026f0aeda4f922
Sorin <sorin.lungu292002@gmail.com>
2022-11-20 23:39:30 +0200

    Add project files.

---
+++ Cub.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+
+namespace Template
+{
+    internal class Cub
+    {
+        private bool myVisibility;
+        private int[,] coordonate=new int[32,3];
+        public Cub()
+        {
+            myVisibility = false;
+
+        }
+        public void Draw()
+        {
+            if (myVisibility)
+            {
+                GL.LineWidth(1.0f);
+                GL.Begin(PrimitiveType.Quads);
+                GL.Color3(Color.Brown);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(0, 0, 20);
+                GL.Vertex3(20, 0, 20);
+                GL.Vertex3(20, 0, 0);
+                GL.Color3(Color.Violet);
+                GL.Vertex3(20, 20, 0);
+                GL.Vertex3(0, 20, 0);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(20,0,0);
+                GL.Color3(Color.Gray);
+                GL.Vertex3(0, 0, 0);
+                GL.Vertex3(0, 0, 20);
+                GL.Vertex3(0, 20, 20);
+                GL.Vertex3(0, 20, 0);
+                GL.Color3(Color.Blue);
+                GL.Vertex3(0, 20, 20);
+                GL.Vertex3(20, 20, 20);
+                GL.Vertex3(20, 0, 20);
+                GL.Vertex3(0, 0, 20);
+                GL.Color3(Color.Green);
+                GL.Vertex3(20,20,20);
+                GL.Vertex3(20,20,0);
+                GL.Vertex3(20, 0, 0);
+                GL.Vertex3(20, 0, 20);
+                GL.Color3(Color.Red);
+                GL.Vertex3(20,20,20);
+                GL.Vertex3(0,20,20);
+                GL.Vertex3(0,20,0);
+                GL.Vertex3(20,20,0);
+                GL.End();
+            }
+        }
+        public void Show()
+        {
+            myVisibility = true;
+        }
+
+        /// <summary>
+        /// Sets visibility of the object OFF.
+        /// </summary>
+        public void Hide()
+        {
+            myVisibility = false;
+        }
+
+        /// <summary>
+        /// Toggles the myVisibility of the object. Once triggered, the attribute is applied automatically on drawing.
+        /// </summary>
+        public void ToggleVisibility()
+        {
+            myVisibility = !myVisibility;
+        }
+        public void CitireFisier(string numeFisier)
+        {
+            int count = 0;
+            string[] lines = File.ReadAllLines(numeFisier);
+            foreach (string line in lines)
+            {
+                string[] continut=line.Split(' ');
+                for(int i = 0; i < continut.Length; i++)
+                {
+                    coordonate[count,i] = Int32.Parse(continut[i]);
+                }
+                count++;
+            }
+        }
+        public void DrawFisier()
+        {
+            GL.Begin(PrimitiveType.Quads);
+            for(int i=0;i<coordonate.GetLength(0);i++)
+            {
+                GL.Vertex3(coordonate[i,0], coordonate[i,1], coordonate[i,2]);
+            }
+            GL.End();
+        }
+        public void MutareX(double factor)
+        {
+            GL.Translate(factor, 0, 0);
+        }
+        public void MutareY(double factor)
+        {
+            GL.Translate(0,factor, 0);
+        }
+        public void MutareZ(double factor)
+        {
+            GL.Translate(0,0,factor);
+        }
+    }
+}
---
+++ Window3D.cs
@@ -0,0 +1,137 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using System;
+using System.Drawing;
+
+namespace Template
+{
+    class Window3D : GameWindow
+    {
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+        const float rotation_speed = 180.0f;
+        float angle;
+        private readonly Axes ax;
+        private Cub cub;
+        double miscareX=0;
+        double miscareY=0;
+        double miscareZ = 0;
+        // DEFAULTS
+        private readonly Color DEFAULT_BKG_COLOR = Color.FromArgb(49, 50, 51);
+        private Vector3 eye = new Vector3(100, 50, 50);
+        private Vector3 target = new Vector3(0, 0, 0);
+        private Vector3 up_vector = new Vector3(0, 1, 0);
+
+        public Window3D() : base(1280, 768, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            // inits
+            ax = new Axes();
+            cub = new Cub();
+            cub.CitireFisier("input.txt");
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // set background
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+
+            // set viewport
+            GL.Viewport(0, 0, this.Width, this.Height);
+
+            // set perspective
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 512);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+
+            // set the eye
+            Matrix4 camera = Matrix4.LookAt(eye, target, up_vector);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape])
+            {
+                Exit();
+            }
+            if (currentKeyboard[Key.T])
+            {
+                cub.Show();
+            }
+            if (currentKeyboard[Key.A])
+            {
+                miscareX--;
+            }
+            if (currentKeyboard[Key.D])
+            {
+                miscareX++;
+            }
+            if (currentKeyboard[Key.W])
+            {
+                miscareY++;
+            }
+            if (currentKeyboard[Key.S])
+            {
+                miscareY--;
+            }
+            if (currentKeyboard[Key.Left])
+            {
+                miscareZ++;
+            }
+            if (currentKeyboard[Key.Right])
+            {
+                miscareZ--;
+            }
+            previousKeyboard = currentKeyboard;
+            previousMouse = currentMouse;
+            // END logic code
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER CODE
+            ax.Draw();
+            GL.PushMatrix();
+            angle += rotation_speed * (float)e.Time;
+            GL.Rotate(angle, 10.1f, 0.0f, .0f);
+            GL.Translate(miscareX, miscareY, miscareZ);
+            cub.DrawFisier();
+            GL.PopMatrix();
+            
+
+
+            // END render code
+
+            SwapBuffers();
+        }
+    }
+}
